769a57a66925d081b1d758fbc0aa9f688e70e3c5
Adrián Martins <40776663+AdrianMartins@users.noreply.github.com>
2018-07-05 21:21:47 -0300

    Clase 2
    Jueves 5/07/2018

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,16 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	
+  var importe;
+  var Resultado;
+  importe =document.getElementById('importe').value;
+  importe =parseInt(importe);
+  resultado =document.getElementById('resultado').value;
+  resultado =parseInt(resultado);
+
+  resultado = (importe * 25) / 100;
+
+  resultado = importe - resultado;
+
+  alert(resultado);
 }
--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,15 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
+   var numeroUno;
+   var numeroDos;
+   var resultado;
+   numeroUno =document.getElementById('numeroUno').value;
+   numeroUno =parseInt(numeroUno);
+   numeroDos =document.getElementById('numeroDos').value;
 
+   resultado = numeroUno + numeroDos;
+
+   alert(resultado);
 }
 

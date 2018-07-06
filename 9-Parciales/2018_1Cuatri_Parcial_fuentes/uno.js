cf8d29910c384d5778c67aae01449d1e4af5f70b
Adrián Martins <40776663+AdrianMartins@users.noreply.github.com>
2018-07-05 22:18:36 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,16 @@
 
 function mostrar()
 {
-
+  var ancho
+  var largo
+  ancho =document.getElementById('ancho').value;
+  largo =document.getElementById('largo').value;
+  
 }
+
+
+
+
+/*se ingresan tres nùmeros informar en un solo alert la suma,
+ el promedio, y la resta de los numeros tal cual 
+ como fueron ingresados */
